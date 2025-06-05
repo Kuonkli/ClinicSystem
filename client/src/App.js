51742b356205fac80a31f9d241d9a75d9e7f67a3
Kuonkli <korotkih1307@mail.ru>
2025-06-05 14:32:58 +0300

    components and pages for the client side

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,28 @@ import './App.css';
 import React from "react";
 import HomePage from "./pages/HomePage";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import DashboardLayout from "./pages/DashboardLayout";
+import {AlertProvider} from "./services/AlertContext";
+import DoctorProfileComponent from "./pages/DoctorProfilePage";
+import DoctorProfilePage from "./pages/DoctorProfilePage";
+import NewAppointmentPage from "./pages/NewAppointmentPage";
 
 function App() {
-  return (
-    <Router>
-        <Routes>
-            <Route path={`/`} element={<HomePage />} />
-        </Routes>
-    </Router>
-  );
+    return (
+        <AlertProvider>
+            <Router>
+                <Routes>
+                    <Route path={`/`} element={<HomePage />} />
+                    <Route path={`/auth`} element={<AuthPage />} />
+                    <Route path={`/dashboard`} element={<DashboardLayout />} />
+                    <Route path={`/dashboard/:content`} element={<DashboardLayout />} />
+                    <Route path={`/dashboard/doctors/:id`} element={<DoctorProfilePage />} />
+                    <Route path={`/dashboard/appointments/new`} element={<NewAppointmentPage />} />
+                </Routes>
+            </Router>
+        </AlertProvider>
+    );
 }
 
 export default App;
